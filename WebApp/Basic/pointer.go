a32110ea283fb911443827a751c66c5fb0803684
dangtrantrung <trung280584@gmail.com>
2024-10-03 06:39:46 +0700

    func applyPointer

--- WebApp/Basic/pointer.go
+++ WebApp/Basic/pointer.go
@@ -9,6 +9,26 @@ func main(){
   fmt.Println(*pointer)
 
   fmt.Println()
-  fmt.Printf("T",pointer)
+  fmt.Printf("%T",pointer)
+  fmt.Println()
+  fmt.Println(a)
+  *pointer=99
+  fmt.Println(a)
+  //pointer->array
 
+  array:=[3]int{1,2,3}
+  var ptn *[3]int
+  ptn=&array
+  fmt.Println(ptn)
+  fmt.Println()
+  fmt.Printf("%T",ptn)
+  fmt.Println()
+
+   c:=45
+   var pointer2 *int=&c
+   applyPointer(pointer2)
+   fmt.Println(c)
+}
+func applyPointer(pointer *int){
+	*pointer=7777
 }
