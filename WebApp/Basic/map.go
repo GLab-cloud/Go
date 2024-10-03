4a0107a37aaa64cda7645588d3b18b272e0c5ba4
dangtrantrung <trung280584@gmail.com>
2024-10-03 11:01:04 +0700

    make map

--- WebApp/Basic/map.go
+++ WebApp/Basic/map.go
@@ -1,5 +1,30 @@
 package main
-import "fmt"
-func main(){
-	
+
+import (
+	"fmt"
+)
+
+func main() {
+	var myMap = make(map[string]int)
+	var myMap1 map[string]int
+	fmt.Println(myMap)
+	if myMap == nil {
+		fmt.Println("mymap==nil")
+	}
+	if myMap1 == nil {
+		fmt.Println("mymap1==nil")
+	}
+	//assign map values
+	myMap2 := map[string]int{
+		"key1": 1,
+		"key2": 2,
+		"key3": 5,
+		"key4": 6,
+	}
+	fmt.Println(myMap2)
+	myMap2["key5"] = 50
+	myMap2["key6"] = 60
+	fmt.Println(myMap2)
+	delete(myMap2, "key1")
+	fmt.Println(myMap2)
 }
