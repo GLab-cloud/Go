77dfbd6fe33aacdbefa6e263d445c0956d84f166
dangtrantrung <trung280584@gmail.com>
2024-10-06 13:26:16 +0700

    Echo - Go Web App framework

--- WebApp/main.go
+++ WebApp/main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	"net/http"
+	"github.com/labstack/echo"
 )
 
-func helloWorldPage(w http.ResponseWriter, r *http.Request ){
-	fmt.Fprint(w,"Hello World")
-}
+// func helloWorldPage(w http.ResponseWriter, r *http.Request ){
+// 	fmt.Fprint(w,"Hello World")
+// }
 
 func main() {
-	http.HandleFunc("/",helloWorldPage)
-	http.ListenAndServe(":8080",nil)
+// 	http.HandleFunc("/",helloWorldPage)
+// 	http.ListenAndServe(":8080",nil)
+e:=echo.New()
+e.GET("/",func(c echo.Context) error{
+	return c.String(http.StatusOK,"Hello, World")
+})
+e.Logger.Fatal(e.Start(":1323"))
 }
