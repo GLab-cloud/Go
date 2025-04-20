29b61baed5a851ca4386ec2ebf6f0f192b98a4b2
dangtrantrung <trung280584@gmail.com>
2025-04-20 16:52:06 +0700

    curl POST form data

--- GIN/webserver.go
+++ GIN/webserver.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -11,20 +10,26 @@ func main(){
 	r.GET("/ping",func (context *gin.Context){
 	// 		context.String(http.StatusOK,"Ping")
 	//  })
-	var data =map[string]interface{}{
-	"message":"Hello from ping",
+	name:=context.DefaultQuery("name","Guest")
+	var data = map[string]interface{}{
+	"message":"Hello "+name+" from ping",
 	}
 	//gin.H{"message":"Hello from ping",}
 
 	context.JSON(http.StatusOK,data)
 	})
 	r.POST("/ping",func(context *gin.Context){
+		address:=context.DefaultPostForm("addr","Viet Nam")
+		age:=context.DefaultPostForm("age","18")
+
 		context.JSON(http.StatusOK,gin.H{
-			"message":"Hello from POST ping",
-			"message 1":"Hello from POST ping 1",
+			"message ":"Hello from" +address+" ping 1",
+			"message 1":"Hello from"+ age +" POST ping",
 
 		})
 	})
+	//curl -X POST "http://localhost:3333/ping" -H "Content-Type: application/x-www-form-urlencoded" -d "addr=TP.HCM&age=41"
+
 	r.GET("/detail/:id",getDetail)
 
 	r.Run(":3333")
