4655c29f9b5aefd4f82e1eae6903078f0aa2e1d4
dangtrantrung <trung280584@gmail.com>
2025-04-20 14:11:00 +0700

    router

--- GIN/webserver.go
+++ GIN/webserver.go
@@ -1,22 +1,37 @@
 package main
 
 import (
+
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 func main(){
- r:=gin.Default()
-r.GET("/ping",func (context *gin.Context){
-// 		context.String(http.StatusOK,"Ping")
-//  })
-var data =map[string]interface{}{
+ 	r:=gin.Default()
+	r.GET("/ping",func (context *gin.Context){
+	// 		context.String(http.StatusOK,"Ping")
+	//  })
+	var data =map[string]interface{}{
 	"message":"Hello from ping",
+	}
+	//gin.H{"message":"Hello from ping",}
+
+	context.JSON(http.StatusOK,data)
+	})
+	r.POST("/ping",func(context *gin.Context){
+		context.JSON(http.StatusOK,gin.H{
+			"message":"Hello from POST ping",
+			"message 1":"Hello from POST ping 1",
+
+		})
+	})
+	r.GET("/detail/:id",getDetail)
+
+	r.Run(":3333")
 }
-//gin.H{"message":"Hello from ping",}
-
-context.JSON(http.StatusOK,data)
-})
-
-r.Run(":3333")
+func getDetail(context *gin.Context){
+	id:=context.Param("id")
+	context.JSON(http.StatusOK,gin.H{
+		"id":id,
+	})
 }
