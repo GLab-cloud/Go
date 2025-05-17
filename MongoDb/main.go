b16ded79a16dd9bf4d5e6ff070bc8851a6be136f
dangtrantrung <trung280584@gmail.com>
2025-05-17 17:03:23 +0700

    mongo-driver connect Mongo Atlas cluster - cloud managed

--- MongoDb/main.go
+++ MongoDb/main.go
@@ -1,41 +1,84 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
+	"log"
+	"os"
+	"time"
 
-	"gopkg.in/mgo.v2"
-
-	"github.com/GLab-cloud/Go/MongoDb/controllers"
-	//"github.com/joho/godotenv"
-	"github.com/julienschmidt/httprouter"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	// "go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-func main(){
-  r:=httprouter.New()
-  uc:=controllers.NewUserController(getSession())
-  r.GET("/user/:id",uc.GetUser ) //curl "http://localhost:3000/user/6828191e7c530c8f2a80c278"
-  r.POST("/user",uc.CreateUser ) //curl -X POST -H 'Content-Type:application/json' http://localhost:3000/user -d '{"Name":"mario", "Gender":"Male","Age":41}'
-  r.DELETE("/user/:id",uc.DeleteUser )
-  http.ListenAndServe("localhost:3000",r)
-
+func main() {
+	godotenv.Load()
+    client, err := mongo.NewClient(options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+    if err != nil {
+        log.Fatal(err)
+    }
+    ctx, _ := context.WithTimeout(context.Background(), 20*time.Second)
+    err = client.Connect(ctx)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer client.Disconnect(ctx)
+    databases, err := client.ListDatabaseNames(ctx, bson.M{})
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Println(databases)
 }
-func getSession() *mgo.Session{
-	//godotenv.Load()
-   url:="mongodb+srv://tintin:9zgXJTFzRbAMgp4A@cluster0.5usukl0.mongodb.net/"
-	// session,err := mgo.Dial(os.Getenv("MONGO_URI"))
-	session,err := mgo.Dial(url)
-	// db:=session.DB("")
-	// names, err := db.CollectionNames()
-	// println(names)
-	if err!=nil{
-		fmt.Printf("Can't connect to mongo, go error %v\n", err)
-		//os.Exit(1)
-	}
-	fmt.Println("connect MONGO Db successful")
 
-	return session
-}
+
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"net/http"
+// 	"os"
+
+// 	"gopkg.in/mgo.v2"
+
+// 	"github.com/GLab-cloud/Go/MongoDb/controllers"
+// 	"github.com/joho/godotenv"
+// 	"github.com/julienschmidt/httprouter"
+// )
+
+// func main(){
+//   r:=httprouter.New()
+//   uc:=controllers.NewUserController(getSession())
+//   r.GET("/user/:id",uc.GetUser ) //curl "http://localhost:3000/user/6828191e7c530c8f2a80c278"
+//   r.POST("/user",uc.CreateUser ) //curl -X POST -H 'Content-Type:application/json' http://localhost:3000/user -d '{"Name":"mario", "Gender":"Male","Age":41}'
+//   r.DELETE("/user/:id",uc.DeleteUser )
+//   http.ListenAndServe("localhost:3000",r)
+
+// }
+// func getSession() *mgo.Session{
+// 	godotenv.Load()
+
+// 	session,err := mgo.Dial(os.Getenv("MONGO_URI"))
+// 	//session,err := mgo.Dial(url)
+// 	// db:=session.DB("")
+// 	// names, err := db.CollectionNames()
+// 	// println(names)
+// 	if err!=nil{
+// 		fmt.Printf("Can't connect to mongo, go error %v\n", err)
+// 		//os.Exit(1)
+// 	}else {
+// 		fmt.Println("connect MONGO Db successful")
+// }
+
+
+// 	return session
+// }
+
+
+
 
 // type userInfo struct {
 // 	username string `bson:"username"`
